246bedd247ccaa142b3ed24342f50a129198f318
salim <groupomania@mail.fr>
2020-08-07 21:53:07 +0200

    modification page d'accueil

--- frontend/src/components/Accueil.vue
+++ frontend/src/components/Accueil.vue
@@ -1,5 +1,12 @@
 <template>
+<div class="Home">
     <h1>Groupomania</h1>
+    <div class="img">
+        <img src="../assets/reseau3.jpg" alt="Photo de la page d'accueil">
+    </div>
+      
+</div>
+    
 </template>
 
 <script>
@@ -16,6 +23,18 @@ h1{
     position: relative;
     bottom: 60px;
     color: #FFF;
+    display: inline;
+    @media screen and (max-width: 1024px){
+        font-size: 1.9em;
+        bottom: 50px;
+    }
+}
+
+img{
+    width: 700px;
+    @media screen and (max-width: 1024px){
+        width: 300px;
+    }
 }
     
 </style>
