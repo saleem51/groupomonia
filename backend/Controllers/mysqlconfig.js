42d26ff956b2c6425c27a32223039fe9e9d83801
saleem51 <saleem51@hotmail.fr>
2021-06-29 23:37:40 +0200

    packages2

--- backend/Controllers/mysqlconfig.js
+++ backend/Controllers/mysqlconfig.js
@@ -5,15 +5,15 @@ dotenv.config({path: './.env'}); //masquage des informations sensibles comme les
 
 const NAME = process.env.NAME;
 const PASS = process.env.PASS;
-const HOST = process.env.HOST;
+const HOST = 'mysql://api_mania_8580:mtn5FuprMGNWwXY1Zbvi@796c2cad-4327-4f50-829b-1fa8ba97268b.api-mania-8580.mysql.dbs.scalingo.com:31741/api_mania_8580?useSSL=true&verifyServerCertificate=false'	
+
 
 //Connexion à la base de donnée Mysql
 
 const db = mysql.createConnection({
     host     : HOST,
-    user     : NAME,
     password : PASS,
-    database: 'Groupomania'
+    database: '$SCALINGO_MYSQL_URL'
 });
 
 db.connect((err) => {
