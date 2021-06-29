dc751666a4ab2228dc74801d2e23e4d671bafb7e
saleem51 <saleem51@hotmail.fr>
2021-06-30 00:10:50 +0200

    changer_database

--- backend/Controllers/mysqlconfig.js
+++ backend/Controllers/mysqlconfig.js
@@ -5,15 +5,16 @@ dotenv.config({path: './.env'}); //masquage des informations sensibles comme les
 
 const NAME = process.env.NAME;
 const PASS = process.env.PASS;
-const HOST = 'mysql://api_mania_8580:mtn5FuprMGNWwXY1Zbvi@796c2cad-4327-4f50-829b-1fa8ba97268b.api-mania-8580.mysql.dbs.scalingo.com:31741/api_mania_8580?useSSL=true&verifyServerCertificate=false'	
-
+// const HOST = 'mysql://api_mania_8580:mtn5FuprMGNWwXY1Zbvi@796c2cad-4327-4f50-829b-1fa8ba97268b.api-mania-8580.mysql.dbs.scalingo.com:31741/api_mania_8580?useSSL=true&verifyServerCertificate=false'	
+const HOST = 'mysql-saleem51.alwaysdata.net';
 
 //Connexion à la base de donnée Mysql
 
 const db = mysql.createConnection({
-    host     : HOST,
-    password : PASS,
-    database: '$SCALINGO_MYSQL_URL'
+    host     : 'mysql-saleem51.alwaysdata.net',
+    password : 'Tutrouverapa26?',
+    database: 'saleem51_groupomania',
+    user: 'saleem51@2a00:b6e0:1:210:1::1'
 });
 
 db.connect((err) => {
