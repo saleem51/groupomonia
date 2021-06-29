da771fdb7684ffbdcc2a12c6bd2801db370c40de
saleem51 <saleem51@hotmail.fr>
2021-06-30 00:35:50 +0200

    listen

--- backend/server.js
+++ backend/server.js
@@ -1,49 +1,55 @@
 const http = require('http');
 const app = require('./app');
+const port = 'https://api-mania.osc-fr1.scalingo.io/';
 
 
-const normalizePort = val => {
-  const port = parseInt(val, 10);
+app.listen(port, function () {
+  console.log('Example app started!')
+})
 
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-};
-const PORT = process.env.PORT;
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+// const normalizePort = val => {
+//   const port = parseInt(val, 10);
 
-const errorHandler = error => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges.');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use.');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
+//   if (isNaN(port)) {
+//     return val;
+//   }
+//   if (port >= 0) {
+//     return port;
+//   }
+//   return false;
+// };
+// const PORT = process.env.PORT;
+// const port = 'https://api-mania.osc-fr1.scalingo.io/';
+// //const port = //normalizePort(process.env.PORT || '3000');
+// app.set('port', port);
 
-const server = http.createServer(app);
+// // const errorHandler = error => {
+// //   if (error.syscall !== 'listen') {
+// //     throw error;
+// //   }
+// //   const address = 'https://api-mania.osc-fr1.scalingo.io/';
+// //   const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+// //   switch (error.code) {
+// //     case 'EACCES':
+// //       console.error(bind + ' requires elevated privileges.');
+// //       process.exit(1);
+// //       break;
+// //     case 'EADDRINUSE':
+// //       console.error(bind + ' is already in use.');
+// //       process.exit(1);
+// //       break;
+// //     default:
+// //       throw error;
+// //   }
+// // };
 
-server.on('error', errorHandler);
-server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
-});
+// const server = http.createServer(app);
 
-server.listen(port);
+// server.on('error', errorHandler);
+// server.on('listening', () => {
+//   const address = server.address();
+//   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+//   console.log('Listening on ' + bind);
+// });
+
+// server.listen(port);
